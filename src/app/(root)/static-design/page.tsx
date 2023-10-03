da35b3d0de54b02b2b3001e7466ac9bfb63111f3
zakariamagdyz <zakariamagdy@ymail.com>
2023-10-03 15:31:39 +0300

    feat: integrate with arabic font

--- src/app/(root)/static-design/page.tsx
+++ src/app/(root)/static-design/page.tsx
@@ -2,13 +2,7 @@ import React from "react";
 
 import { tajawalFont } from "@/utils/fonts";
 const StaticDesignPage = () => {
-  return (
-    <div>
-      <h1 className="text-4xl font-bold ">الحمد لله رب العالمين</h1>
-      <h2 className="text-4xl font-medium">صل الله عليه</h2>
-      <p className="text-2xl font-regular">اشترك الان</p>
-    </div>
-  );
+  return <main></main>;
 };
 
 export default StaticDesignPage;
